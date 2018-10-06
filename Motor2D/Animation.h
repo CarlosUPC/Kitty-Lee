076317f6182d0098a581a5b4bae7378a7e65301f
CarlosUPC <wolfsea.yt@gmail.com>
2018-10-06 18:27:10 +0200

    Added Player Module + Anim.h

---
+++ Motor2D/Animation.h
@@ -0,0 +1,80 @@
+#ifndef __ANIMATION_H__
+#define __ANIMATION_H__
+
+#include "SDL/include/SDL_rect.h"
+#include "p2Point.h"
+#include "p2DynArray.h"
+
+struct FrameList {
+	SDL_Rect rect;
+	//iPoint pivot;
+};
+
+class Animation
+{
+public:
+	bool loop = true;
+	float speed = 1.0f;
+	p2DynArray<FrameList> frames;
+
+private:
+	float current_frame = 0.0f;
+	int last_frame = 0;
+	int loops = 0;
+
+public:
+
+	Animation()
+	{}
+
+	Animation(const Animation& anim) : loop(anim.loop), speed(anim.speed), last_frame(anim.last_frame), frames(anim.frames)
+	{
+		//SDL_memcpy(&frames, anim.frames, sizeof(frames));
+	}
+
+	void PushBack(const SDL_Rect& rect)
+	{
+		frames[last_frame++] = { rect };
+	}
+
+	FrameList& GetCurrentFrame()
+	{
+		current_frame += speed;
+		if (current_frame >= last_frame)
+		{
+			current_frame = (loop) ? 0.0f : last_frame - 1;
+			loops++;
+		}
+
+		return frames[(int)current_frame];
+	}
+
+	bool Finished() const
+	{
+		return loops > 0;
+	}
+
+	void Reset()
+	{
+		current_frame = 0.0f;
+		loops = 0;
+		
+	}
+
+	int getFrameIndex() const
+	{
+		return (int)current_frame;
+	}
+
+	bool isDone() {
+		if (current_frame == last_frame - 1) return true;
+		else return false;
+	}
+
+	int GetCurrentLoop() const
+	{
+		return loops;
+	}
+};
+
+#endif
---
+++ Motor2D/j1Player.cpp
@@ -0,0 +1,66 @@
+#include "j1Player.h"
+#include "p2Defs.h"
+#include "p2Log.h"
+
+
+j1Player::j1Player() {
+
+}
+
+
+
+j1Player::~j1Player()
+{
+}
+
+void j1Player::Init()
+{
+	active = true;
+}
+
+// Called before render is available
+bool j1Player::Awake(pugi::xml_node&)
+{
+	LOG("Loading Player Module");
+	return true;
+}
+
+// Called before the first frame
+bool j1Player::Start()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Player::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Player::Update(float dt)
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Player::PostUpdate()
+{
+	return true;
+}
+
+// Called before quitting
+bool j1Player::CleanUp()
+{
+	return true;
+}
+
+bool j1Player::Load(pugi::xml_node&)
+{
+	return true;
+}
+
+bool j1Player::Save(pugi::xml_node&) const
+{
+	return true;
+}

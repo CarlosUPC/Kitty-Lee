1b5481b1475d9409c35a8b6e25742a2aa29cdf37
Christian Martínez <christt105@gmail.com>
2018-10-14 12:56:10 +0200

    Added max acceleration Y to don't fall at a high speed and have problems with collisions

--- Motor2D/j1Map.cpp
+++ Motor2D/j1Map.cpp
@@ -379,6 +379,8 @@ void j1Map::LoadProperties(pugi::xml_node& properties_node) {
 		nameProperty = properties_node.attribute("name").as_string();
 		if (nameProperty == "gravity")
 			data.gravity = properties_node.attribute("value").as_float();
+		if (nameProperty == "maxAccelerationY")
+			data.maxAccelerationY = properties_node.attribute("value").as_float();
 		if (nameProperty == "musicEnvironment") 
 			data.musicEnvironment = properties_node.attribute("value").as_string();
 		

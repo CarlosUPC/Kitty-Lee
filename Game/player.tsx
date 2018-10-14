496a5c808f12e3ad0715875badb3166c81074b04
CarlosUPC <wolfsea.yt@gmail.com>
2018-10-14 12:34:08 +0200

    gauzy state anims id = 4 & 24

--- Game/player.tsx
+++ Game/player.tsx
@@ -25,6 +25,14 @@
    <frame tileid="3" duration="100"/>
   </animation>
  </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
  <tile id="16">
   <animation>
    <frame tileid="16" duration="100"/>
@@ -35,6 +43,18 @@
    <frame tileid="21" duration="100"/>
    <frame tileid="22" duration="100"/>
    <frame tileid="23" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
   </animation>
  </tile>
  <tile id="32">

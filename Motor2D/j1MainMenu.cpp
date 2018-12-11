eaff7fb937412cbac49d595d6e4e1b09d7f04b51
CarlosUPC <wolfsea.yt@gmail.com>
2018-12-11 19:47:09 +0100

    added menu empty modules & working on fadetoblack

---
+++ Motor2D/j1MainMenu.cpp
@@ -0,0 +1,95 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Map.h"
+#include "j1MainMenu.h"
+#include "j1Scene.h"
+#include "j1Collision.h"
+#include "j1FadeToBlack.h"
+#include "j1Pathfinding.h"
+#include "Player.h"
+#include "j1Gui.h"
+#include "Image.h"
+#include "Button.h"
+#include "Label.h"
+
+
+
+j1MainMenu::j1MainMenu() : j1Module()
+{
+	name.create("main_menu");
+}
+
+// Destructor
+j1MainMenu::~j1MainMenu()
+{}
+
+// Called before render is available
+
+bool j1MainMenu::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1MainMenu::Start()
+{
+	bool ret = true;
+
+	return ret;
+}
+
+// Called each loop iteration
+bool j1MainMenu::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1MainMenu::Update(float dt)
+{
+	
+	return true;
+}
+
+// Called each loop iteration
+bool j1MainMenu::PostUpdate()
+{
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before quitting
+bool j1MainMenu::CleanUp()
+{
+	LOG("Freeing main_menu");
+	
+	return true;
+}
+
+bool j1MainMenu::Load(pugi::xml_node& load)
+{
+	bool ret = true;
+
+	
+
+	return ret;
+}
+
+bool j1MainMenu::Save(pugi::xml_node& save) const
+{
+	bool ret = true;
+
+
+	return ret;
+}
+
---
+++ Motor2D/j1MainMenu.h
@@ -0,0 +1,50 @@
+#ifndef __J1MAINMENU_H__
+#define __J1MAINMENU_H__
+
+#include "j1Module.h"
+#include "p2DynArray.h"
+
+class Button;
+class Label;
+class Image;
+
+
+class j1MainMenu : public j1Module
+{
+public:
+
+	j1MainMenu();
+
+	// Destructor
+	virtual ~j1MainMenu();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	bool Load(pugi::xml_node&);
+
+	bool Save(pugi::xml_node&) const;
+
+
+public:
+
+
+private:
+
+};
+#endif // __j1MAINMENU_H__

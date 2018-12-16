b121bb1f377b3e1f9391e956314fd883c7246ede
Christian Martínez <christt105@gmail.com>
2018-12-16 11:49:26 +0100

    set string team and license in one label

--- Motor2D/j1MainMenu.h
+++ Motor2D/j1MainMenu.h
@@ -74,8 +74,8 @@ private:
 	Label* credits_lbl = nullptr;
 	Label* settings_lbl = nullptr;
 	
+	Image* clip_credits = nullptr;
 	Label* license_lbl = nullptr;
-	Label* team_lbl = nullptr;
 	Label* tasks_lbl = nullptr;
 
 	p2DynArray<Label*> labels;
